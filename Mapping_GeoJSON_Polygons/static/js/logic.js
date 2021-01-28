34afea2d9b7b466df2b4530ac3f2a360fec243f3
crashinabhi <avhad.abhijit@yahoo.com>
2021-01-27 22:35:42 -0500

    Added torontoNeighborhoods.json file

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -110,10 +110,22 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/crashinabhi/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+    style: myStyle,      
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+        + feature.properties.dst +"</h3>");
+    }
+})
+.addTo(map);
 });
-
